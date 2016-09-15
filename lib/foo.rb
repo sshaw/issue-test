ab9bea36316a3cc60c974e4196c0a562f127c0c5
sshaw <skye.shaw@gmail.com>
2016-09-15 00:13:33 -0400

    violate some rules

--- lib/foo.rb
+++ lib/foo.rb
@@ -6,5 +6,27 @@ class Foo
   def baz
     a = { :a => 123 }
     return if a == 1 or b == 2
+    10 ** 10
+  end
+
+  def baz(n)
+    a = { :a => 123 }
+    return if a == 1 or b == 2
+    a = { :a => 123 }
+    return if a == 1 or b == 2
+    a = { :a => 123 }
+    return if a == 1 or b == 2
+    a = { :a => 123 }
+    return if a == 1 or b == 2
+    a = { :a => 123 }
+    return if a == 1 or b == 2
+    a = { :a => 123 }
+    return if a == 1 or b == 2
+    a = { :a => 123 }
+    return if a == 1 or b == 2
+    a = { :a => 123 }
+    return if a == 1 or b == 2
+
+    n < 10 ? true : false
   end
 end
